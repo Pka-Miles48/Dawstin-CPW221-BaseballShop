6a0ff9a80905f342e8ab5b3f19a66dc7aad315b1
Dawstin Waters <dawstinw4@gmail.com>
2025-04-11 04:07:04 -0700

    Added key files and code for the data and models

---
+++ Dawstin-CPW221-BaseballShop/Baseball Data/Item.cs
@@ -0,0 +1,7 @@
+﻿namespace Dawstin_CPW221_BaseballShop.Baseball_Data
+{
+    public class Item
+    {
+        public object Category { get; internal set; }
+    }
+}
---
+++ Dawstin-CPW221-BaseballShop/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace Dawstin_CPW221_BaseballShop.Models
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<Item> Items { get; set; }
+    }
+}

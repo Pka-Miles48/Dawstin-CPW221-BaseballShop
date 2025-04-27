b2f6a6949a6e2b8065f7100c0d947492fb28f66f
Dawstin Waters <dawstinw4@gmail.com>
2025-04-27 00:50:19 -0700

    Updated database schema and used XML documentation comments

--- Dawstin-CPW221-BaseballShop/Models/Category.cs
+++ Dawstin-CPW221-BaseballShop/Models/Category.cs
@@ -1,10 +1,28 @@
 ﻿namespace Dawstin_CPW221_BaseballShop.Models
 {
+    /// <summary>
+    /// Represents a product category in the Baseball Shop.
+    /// </summary>
     public class Category
     {
+        /// <summary>
+        /// Gets or sets the unique identifier for the category.
+        /// </summary>
         public int CategoryID { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the category.
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        /// Gets or sets the collection of items associated with this category.
+        /// </summary>
         public ICollection<Item> Items { get; set; }
+
+        /// <summary>
+        /// Gets or sets the collection of products belonging to this category.
+        /// </summary>
+        public ICollection<Product> Products { get; set; }
     }
 }

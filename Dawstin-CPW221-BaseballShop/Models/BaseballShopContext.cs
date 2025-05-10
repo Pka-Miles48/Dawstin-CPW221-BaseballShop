4ac057c44cb714d4872e426703ec665ac21e93d2
Dawstin Waters <dawstinw4@gmail.com>
2025-05-10 00:03:36 -0700

    Update Your BaseballShopContext

---
+++ Dawstin-CPW221-BaseballShop/Models/BaseballShopContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dawstin_CPW221_BaseballShop.Models
+{
+    public class BaseballShopContext : IdentityDbContext<ApplicationUser>
+    {
+        public BaseballShopContext(DbContextOptions<BaseballShopContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+
+    public class ApplicationUser : IdentityUser
+    {
+        // Add custom fields if needed
+    }
+}

d8cc37da9c37769208786a3efa5f7fa6e049b278
Dawstin Waters <dawstinw4@gmail.com>
2025-05-10 00:15:39 -0700

    Created Account folder and files; created new View Models

---
+++ Dawstin-CPW221-BaseballShop/Models/ManageAccountViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Dawstin_CPW221_BaseballShop.Models
+{
+    public class ManageAccountViewModel
+    {
+        public string Email { get; set; }
+        public string NewPassword { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}

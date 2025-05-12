eeef54035483b92573b1131e626bee783091c268
Dawstin Waters <dawstinw4@gmail.com>
2025-05-11 21:31:58 -0700

    Designed the Admin Dashboard View

---
+++ Dawstin-CPW221-BaseballShop/Models/AdminDashboardViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Dawstin_CPW221_BaseballShop.Models
+{
+    internal class AdminDashboardViewModel
+    {
+        public List<Product> Products { get; set; }
+        public List<Order> Orders { get; set; }
+    }
+}

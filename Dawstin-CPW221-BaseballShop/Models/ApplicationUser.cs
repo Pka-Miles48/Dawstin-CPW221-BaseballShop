e743ec004cb793f0eeee88f028d06032233e6a70
Dawstin Waters <dawstinw4@gmail.com>
2025-05-11 21:34:31 -0700

    Modified ApplicationUser.cs file

---
+++ Dawstin-CPW221-BaseballShop/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Dawstin_CPW221_BaseballShop.Models
+{
+    /// <summary>
+    /// Represents an application user with authentication details.
+    /// </summary>
+    public class ApplicationUser : IdentityUser
+    {
+        // You can add custom properties here if needed
+    }
+}

aaf767e6496a976bb6511afe177c3487451578fb
Dawstin Waters <dawstinw4@gmail.com>
2025-05-14 02:03:13 -0700

    Updated settings and Program.cs

--- Dawstin-CPW221-BaseballShop/Program.cs
+++ Dawstin-CPW221-BaseballShop/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<BaseballShop>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BaseballShop")));
 
 builder.Services.AddControllersWithViews();
 
